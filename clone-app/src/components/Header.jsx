59fa760d5b68907f24a5c11a3c1de19e712c15b1
Benny-Nwaro <baroh3767@aol.com>
2023-08-22 15:50:34 +0100

    updated

--- clone-app/src/components/Header.jsx
+++ clone-app/src/components/Header.jsx
@@ -4,14 +4,18 @@ function Car() {
     return (
 
       <div className='main-container'> 
-        <div>
+        <div className='nav-bars'>
+        <img src="./logo-removebg-preview.png" alt="React Image" />
           
           <button type="button" className='btn'>Home</button>
 
           <button type="button" className='btn'>Redeem</button>
 
           <button type="button" className='btn'>My movies</button>
-
+        </div>
+        
+        <div className='register'>
+          <button type="submit"> </button>
           <button type="button" className='btn'>Sign In</button>
           <button type="button" className='btn2'>Sign Up Now</button>
         </div>

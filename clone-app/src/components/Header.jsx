6768bc4796bd5be6901d6c2a4b99f97aa54dd5ac
Benny-Nwaro <baroh3767@aol.com>
2023-08-22 14:30:36 +0100

    update

---
+++ clone-app/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import './header.css'
+
+function Car() {
+    return (
+
+      <div className='main-container'> 
+        <div>
+          
+          <button type="button" className='btn'>Home</button>
+
+          <button type="button" className='btn'>Redeem</button>
+
+          <button type="button" className='btn'>My movies</button>
+
+          <button type="button" className='btn'>Sign In</button>
+          <button type="button" className='btn2'>Sign Up Now</button>
+        </div>
+          
+
+      </div> 
+      
+    )
+
+     
+   
+   
+  
+
+  }
+  
+  export default Car;
